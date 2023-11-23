a268e681c7a27b2824e8a2bea0940bf1fbcbdedf
sam06tanb <samirtajra53@gmail.com>
2023-11-22 22:34:03 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        method1();
+        System.out.println("End of program!");
+    }
+    public static void method1() {
+     System.out.println("METHOD1 START!");
+     method2();
+     System.out.println("METHOD1 END!");
+    }
+        public static void method2() {
+            System.out.println("METHOD2 START!");
+            Scanner sc = new Scanner(System.in);
+
+            try {
+                String[] vect = sc.nextLine().split(" ");
+                int position = sc.nextInt();
+                System.out.println(vect[position]);
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Do not have this vector position!");
+                e.printStackTrace();
+                sc.next();
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Only numbers!");
+            }
+            sc.close();
+            System.out.println("METHOD2 END!");
+        }
+
+
+        }
+
